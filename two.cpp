1bcb17cd11532bfdb731ce1d56ba92e4890930e2
Tushar Saxena <stushar345@gmail.com>
2021-03-16 09:54:07 +0530

    Add files via upload

---
+++ two.cpp
@@ -0,0 +1,17 @@
+int getPairsCount(int arr[], int n, int k) 
+    {
+         int c=0;
+        unordered_map<int,int> m;
+    for(int i=0;i<n;i++)
+    {
+        if(m.find(k-arr[i])!=m.end())
+        {
+            c+=m[k-arr[i]]; 
+        }
+        
+        m[arr[i]]++;
+        
+    }
+    return c;
+    }
+};
